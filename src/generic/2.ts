39d2205bd4c5e6b980cefede8cf59701cf349f77
boogie1989 <serhiibuhai.work@gmail.com>
2024-10-22 14:46:27 +0300

    initial

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -5,19 +5,19 @@
 */
 
 type AllType = {
-  name: string;
-  position: number;
-  color: string;
-  weight: number
-}
+	name: string;
+	position: number;
+	color: string;
+	weight: number;
+};
 
-function compare (top, bottom): AllType {
-  return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  }
+function compare<T extends Pick<AllType, keyof AllType>>(top: T, bottom: T): AllType {
+	return {
+		name: top.name,
+		color: top.color,
+		position: bottom.position,
+		weight: bottom.weight,
+	};
 }
 
 export {};
--- src/generic/6.ts
+++ src/generic/6.ts
@@ -8,16 +8,14 @@
 */
 
 type User = {
-  name: string;
-  surname: string;
-  email: string;
-  password: string;
-}
+	name: string;
+	surname: string;
+	email: string;
+	password: string;
+};
 
-function createOrUpdateUser(initialValues: User) {
-  // Оновлення користувача
-}
+function createOrUpdateUser(initialValues: Partial<User>) {}
 
-createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
+createOrUpdateUser({ email: "user@mail.com", password: "password123" });
 
 export {};
